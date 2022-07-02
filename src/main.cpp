c3b2838ca618cde48595a51ba351da89cda683d0
John <johnmilanowski@gmail.com>
2022-07-01 19:43:37 -0700

    Add button press and rotary features. Add many new palettes.

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #define ROTARY_ENCODER_A_PIN 32
 #define ROTARY_ENCODER_B_PIN 33
 #define ROTARY_ENCODER_VCC_PIN -1
-#define ROTARY_ENOCDER_STEPS 5
+#define ROTARY_ENOCDER_STEPS 4
 
 #define encBtn 27
 
@@ -16,14 +16,18 @@
 #define LED_STRIP_B_PIN 18
 #define NUM_LEDS 300
 
-enum State { BRIGHTNESS, COLOR, PALETTE };
+enum STATES { SPEED, DENSITY, PALETTE };
 
 CRGB ledStripA[NUM_LEDS] = {0};
 CRGB ledStripB[NUM_LEDS] = {0};
-int brightness = 60;
-int brightnessMax = 120;
+int brightness = 100;
+int brightnessMax = 100;
 int powerLimit = 5000;
 int paletteIndex = 0;
+int speed = 10;
+float density = 1;
+int paletteId = 0;
+int state = 0;
 
 #include "palettes.h"
 
@@ -35,22 +39,49 @@ OneButton btn = OneButton (
     true
 );
 
+float readEncoderFloat() {
+  return (float)rotaryEncoder.readEncoder() / 100.0;
+}
+
 static void handleClick() {
-  Serial.println("Single Clicked");
+  Serial.println("Single Clicked: Entering speed mode");
+  state = STATES::SPEED;
+  rotaryEncoder.setBoundaries(0, 100, false);
+  rotaryEncoder.setEncoderValue(speed);
+  rotaryEncoder.setAcceleration(25);
 }
 
 static void handleDoubleClick() {
-  Serial.println("Double Clicked!");
+  Serial.println("Double Clicked: Entering density mode");
+  state = STATES::DENSITY;
+  rotaryEncoder.setBoundaries(1 * 100, 10 * 100, false);
+  int densityInt = density * 100;
+  rotaryEncoder.setEncoderValue(densityInt);
+  rotaryEncoder.setAcceleration(400);
 }
 
 static void handleLongClick() {
-  Serial.println("Long clicked!");
+  Serial.println("Long clicked: Entering palette/color change mode");
+  state = STATES::PALETTE;
+  rotaryEncoder.setBoundaries(0, 7, true);
+  rotaryEncoder.setEncoderValue(paletteId);
+  rotaryEncoder.setAcceleration(0);
 }
 
 void rotary_loop() {
   if (rotaryEncoder.encoderChanged()) {
     Serial.print("Value: ");
-    Serial.println(rotaryEncoder.readEncoder());
+
+    if (state == STATES::SPEED) {
+      Serial.println(rotaryEncoder.readEncoder());
+      speed = rotaryEncoder.readEncoder();
+    } else if (state == STATES::DENSITY) {
+      Serial.println(readEncoderFloat());
+      density = readEncoderFloat();
+    } else {
+      Serial.println(rotaryEncoder.readEncoder());
+      paletteId = rotaryEncoder.readEncoder();
+    }
   }
 }
 
@@ -67,8 +98,8 @@ void setup() {
   rotaryEncoder.begin();
   rotaryEncoder.setup(readEncoderISR);
   bool circleValues = false;
-  rotaryEncoder.setBoundaries(0, 1000, circleValues);
-  rotaryEncoder.setAcceleration(250);
+  rotaryEncoder.setBoundaries(0, 100, circleValues);
+  rotaryEncoder.setAcceleration(25);
 
   // Setup Buttons
   btn.attachClick(handleClick);
@@ -84,12 +115,36 @@ void setup() {
 }
 
 void loop() {
-  rotary_loop();
   btn.tick();
-  
-  fill_rainbow(ledStripA, NUM_LEDS, paletteIndex, 2.55);
-  fill_rainbow(ledStripB, NUM_LEDS, paletteIndex, 2.55);
-  FastLED.delay(5);
+  rotary_loop();
+
+  if (paletteId == 0) {
+    fill_palette(ledStripA, NUM_LEDS, paletteIndex, density, sunset1, 100, LINEARBLEND);
+    fill_palette(ledStripB, NUM_LEDS, paletteIndex, density, sunset1, 100, LINEARBLEND);
+  } else if (paletteId == 1) {
+    fill_palette(ledStripA, NUM_LEDS, paletteIndex, density, sunset2, 100, LINEARBLEND);
+    fill_palette(ledStripB, NUM_LEDS, paletteIndex, density, sunset2, 100, LINEARBLEND);
+  } else if (paletteId == 2) {
+    fill_palette(ledStripA, NUM_LEDS, paletteIndex, density, sanjaun, 100, LINEARBLEND);
+    fill_palette(ledStripB, NUM_LEDS, paletteIndex, density, sanjaun, 100, LINEARBLEND);
+  } else if (paletteId == 3) {
+    fill_palette(ledStripA, NUM_LEDS, paletteIndex, density, fire, 100, LINEARBLEND);
+    fill_palette(ledStripB, NUM_LEDS, paletteIndex, density, fire, 100, LINEARBLEND);
+  } else if (paletteId == 4) {
+    fill_palette(ledStripA, NUM_LEDS, paletteIndex, density, outrun1, 100, LINEARBLEND);
+    fill_palette(ledStripB, NUM_LEDS, paletteIndex, density, outrun1, 100, LINEARBLEND);
+  } else if (paletteId == 5) {
+    fill_palette(ledStripA, NUM_LEDS, paletteIndex, density, outrun2, 100, LINEARBLEND);
+    fill_palette(ledStripB, NUM_LEDS, paletteIndex, density, outrun2, 100, LINEARBLEND);
+  } else if (paletteId == 6) {
+    fill_palette(ledStripA, NUM_LEDS, paletteIndex, density, outrun3, 100, LINEARBLEND);
+    fill_palette(ledStripB, NUM_LEDS, paletteIndex, density, outrun3, 100, LINEARBLEND);
+  } else if (paletteId == 7) {
+    fill_rainbow(ledStripA, NUM_LEDS, paletteIndex, density);
+    fill_rainbow(ledStripB, NUM_LEDS, paletteIndex, density);
+  }
+
+  FastLED.delay(speed);
   paletteIndex++;
   FastLED.setBrightness(brightness);
 }
--- src/palettes.h
+++ src/palettes.h
@@ -4,11 +4,63 @@
 #include <FastLED.h>
 
 DEFINE_GRADIENT_PALETTE (sunset1_gp) {
-  0, 136, 205, 200, // Teal
-  51, 233, 178, 159, // Peach
-  102, 254, 153, 127, // Orange
-  180, 237, 117, 179, // Pink
-  255, 135, 98, 120 // Purple
+  0, 90, 206, 198, // Teal
+  51, 232, 131, 97, // Peach
+  102, 242, 113, 78, // Orange
+  180, 234, 65, 152, // Pink
+  255, 124, 44, 92 // Purple
+};
+
+DEFINE_GRADIENT_PALETTE (sunset2_gp) {
+  0, 181, 61, 173, // Lavender
+  170, 216, 71, 98, // Peach
+  255, 249, 123, 64// Orange
+};
+
+DEFINE_GRADIENT_PALETTE (sanjaun_gp) {
+  0, 99, 168, 211, // Teal
+  51, 221, 139, 146, // Pinkish gray
+  102, 126, 106, 168, // Bluish gray
+  180, 77, 137, 137, // Pale teal
+  255, 22, 78, 79
+};
+
+DEFINE_GRADIENT_PALETTE (fire_gp) {
+  0, 49, 22, 21,  // Dark Red
+  51, 103, 35, 21,
+  120, 163, 55, 23,
+  200, 224, 100, 22,
+  255, 253, 150, 23
+};
+
+DEFINE_GRADIENT_PALETTE (outrun1_gp) {
+  0, 192, 6, 172, // Purple 1
+  37, 160, 7, 182, // Purple 2
+  74, 99, 49, 192, // Purple 
+  105, 75, 69, 191, //Purple blue
+  136, 20, 109, 197, //Blue 1
+  167, 14, 144, 206, // Blue 2
+  255, 9, 170, 204   // Blue 3
+};
+
+DEFINE_GRADIENT_PALETTE (outrun2_gp) {
+  0, 94, 49, 150, // Purple
+  75, 34, 91, 170, // Blue
+  140, 248, 190, 21, // Yellow
+  200, 242, 93, 50, // Orange
+  255, 252, 21, 128, // Pink
+};
+
+DEFINE_GRADIENT_PALETTE (outrun3_gp) {
+  0, 139, 242, 238, // Teal
+  125, 223, 115, 198, // Pink
+  255, 165, 90, 247 // Purple 
 };
 
 CRGBPalette16 sunset1 = sunset1_gp;
+CRGBPalette16 sunset2 = sunset2_gp;
+CRGBPalette16 sanjaun = sanjaun_gp;
+CRGBPalette16 fire = fire_gp;
+CRGBPalette16 outrun1 = outrun1_gp;
+CRGBPalette16 outrun2 = outrun2_gp;
+CRGBPalette16 outrun3 = outrun3_gp;
